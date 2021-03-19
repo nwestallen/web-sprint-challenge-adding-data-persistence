78d0cca63dc934fe92b9dd06e7881b63f51b3e01
Nick <nwestallen@gmail.com>
2021-03-19 15:51:32 -0400

    hooked up tasks router

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
 // build your server here and require it from index.js
 const express = require('express');
 
-const ResourceRouter = require('./resource/router')
-const ProjectRouter = require('./project/router')
+const ResourceRouter = require('./resource/router');
+const ProjectRouter = require('./project/router');
+const TaskRouter = require('./task/router');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/resources', ResourceRouter);
 server.use('/api/projects', ProjectRouter);
+server.use('/api/tasks', TaskRouter);
 
 module.exports = server;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,15 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const Task = require('./model');
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Task.getAll()
+    .then(task => {
+        res.json(task);
+    })
+    .catch(next)
+});
+
+module.exports = router;
