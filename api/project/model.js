4662c3c26e7e3f5f1675585c5293fd4ce861bf32
Nick <nwestallen@gmail.com>
2021-03-19 15:41:32 -0400

    projects endpoints

--- api/project/model.js
+++ api/project/model.js
@@ -5,6 +5,19 @@ const getAll = () => {
     return db('projects');
 };
 
+const getById = project_id => {
+    return db('projects')
+    .where({ project_id })
+    .first()
+};
+
+const create = project => {
+    return db('projects')
+    .insert(project)
+    .then(id => getById(id));
+};
+
 module.exports = {
-    getAll
+    getAll,
+    create
 }
--- api/project/router.js
+++ api/project/router.js
@@ -7,10 +7,24 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     Project.getAll()
     .then(projects => {
-        res.json(projects);
+        res.json(projects.map(project => ({
+            ...project, 
+            project_completed: !!project.project_completed
+        })));
     })
     .catch(next);
 });
+
+router.post('/', (req, res, next) => {
+    Project.create(req.body)
+    .then(project => {
+        res.json({
+            ...project, 
+            project_completed: !!project.project_completed
+        });
+    })
+    .catch(next);
+})
 
 router.use((err, req, res) => {
     res.status(500).json({
