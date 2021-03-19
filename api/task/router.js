66723a7c92691d50c85668875d5d71e1addb1192
Nick <nwestallen@gmail.com>
2021-03-19 16:06:44 -0400

    tasks endpoints

--- api/task/router.js
+++ api/task/router.js
@@ -6,10 +6,31 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
     Task.getAll()
-    .then(task => {
-        res.json(task);
+    .then(tasks => {
+        res.json(tasks.map(task => ({
+            ...task,
+            task_completed: !!task.task_completed
+        })));
     })
     .catch(next)
+});
+
+router.post('/', (req, res, next) => {
+    Task.create(req.body)
+    .then(task => {
+        res.json({
+            ...task,
+            task_completed: !!task.task_completed
+        });
+    })
+    .catch(next)
+});
+
+router.use((err, req, res) => {
+    res.status(500).json({
+        error: err.message,
+        stack: err.stack
+    })
 });
 
 module.exports = router;
