9e42df443d11c1acee5b2447e305b79c9aa835cd
Nick <nwestallen@gmail.com>
2021-03-19 12:36:38 -0400

    migration file setup

---
+++ data/migrations/20210319144054_build_schema.js
@@ -0,0 +1,33 @@
+
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('projects', tbl => {
+          tbl.increments('project_id')
+          tbl.string('project_name').notNullable()
+          tbl.string('project_description')
+          tbl.boolean('project_completed').defaultTo(false)
+      })
+      .createTable('resources', tbl => {
+          tbl.increments('resource_id')
+          tbl.string('resource_name').notNullable().unique()
+          tbl.string('resource_description')
+      })
+      .createTable('tasks', tbl => {
+          tbl.increments('task_id')
+          tbl.string('task_description').notNullable()
+          tbl.string('task_notes')
+          tbl.boolean('task_completed').defaultTo(false)
+          tbl.integer('project_id').notNullable().references('project_id').inTable('projects')
+      })
+      .createTable('project_resources', tbl => {
+          tbl.increments('project_resource_id')
+      })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
